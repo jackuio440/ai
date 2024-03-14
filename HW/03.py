fe9c602b17573e4592955e5d9e14a264bd6a25a9
jackuio440 <83624528+jackuio440@users.noreply.github.com>
2024-03-14 13:51:32 +0800

    Create 03.py

---
+++ HW/03.py
@@ -0,0 +1,22 @@
+import random  
+def maximize():
+    x = 0
+    y = 0
+    z = 0
+    for i in range(10000):
+        
+        x2 = random.uniform(0,10)  
+        y2 = random.uniform(0,10)  
+        z2 = random.uniform(0,5.5)  
+        if(x2+y2<=10 and 2*x2+z2<=9 and y2+2*z2<=11):
+            if 3*x2+2*y2+5*z2>=3*x+2*y+5*z:
+                x=x2
+                y=y2
+                z=z2
+                print(x,y,z)
+    ans=3*x+2*y+5*z
+    return {"x": x, "y": y, "z": z}, ans
+
+
+solution, ans = maximize()
+print(solution,ans)
