93c11dd9cf276121edddbe496d224980d9588584
jackuio440 <83624528+jackuio440@users.noreply.github.com>
2024-04-25 11:45:07 +0800

    Update lenet.py

--- HW/07/lenet.py
+++ HW/07/lenet.py
@@ -5,7 +5,7 @@ import sys
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 5, kernel_size=3)
+        self.conv1 = nn.Conv2d(1, 5, kernel_size=3)   //將kernal_size修改至就可以得到97%的正確率
         self.conv2 = nn.Conv2d(5, 10, kernel_size=3)
         self.fc1 = nn.Linear(160, 50)
         self.fc2 = nn.Linear(50, 10)
